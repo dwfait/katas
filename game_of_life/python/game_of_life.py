73182ccfb8d15854440ddbb66b3abcfe60f94b39
Dwain Faithfull <dwfaithfull@gmail.com>
2019-10-14 21:56:46 +0100

    Getting/setting cells in PyGoL

--- game_of_life/python/game_of_life.py
+++ game_of_life/python/game_of_life.py
@@ -2,6 +2,11 @@ class GameOfLife():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.cells = [[False for i in range(y)] for j in range(x)]
 
-    def test(self):
-        return 3
+    def get_cell(self, x, y):
+        return self.cells[x][y]
+
+    def set_cell(self, x, y, value):
+        self.cells[x][y] = value
+
--- game_of_life/python/test.py
+++ game_of_life/python/test.py
@@ -5,6 +5,21 @@ class TestGameOfLife(unittest.TestCase):
     def test_board_initialisation(self):
         game_of_life.GameOfLife(10, 10)
 
+    def test_initial_cell_state_is_dead(self):
+        board = game_of_life.GameOfLife(1, 1)
+        self.assertEqual(board.get_cell(0, 0), False)
+
+    def test_cell_can_be_stimulated(self):
+        board = game_of_life.GameOfLife(1, 1)
+        board.set_cell(0, 0, True)
+        self.assertEqual(board.get_cell(0, 0), True)
+
+    def test_larger_board_stimulating(self):
+        board = game_of_life.GameOfLife(5, 5)
+        board.set_cell(4, 3, True)
+        self.assertEqual(board.get_cell(4, 3), True)
+        self.assertEqual(board.get_cell(3, 3), False)
+
 if __name__ == '__main__':
   unittest.main()
 
