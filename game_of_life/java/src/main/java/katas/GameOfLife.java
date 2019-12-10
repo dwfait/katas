c9659d7fe3a4e74504f86ca91d071d5b2d3bc535
Dwain Faithfull <dwfaithfull@gmail.com>
2019-12-10 18:23:57 +0000

    Java implementation

--- game_of_life/java/src/main/java/katas/GameOfLife.java
+++ game_of_life/java/src/main/java/katas/GameOfLife.java
@@ -1,11 +1,15 @@
 package katas;
 
+import java.util.ArrayList;
+
 public class GameOfLife {
-  int x, y;
+  private ArrayList<ArrayList<Boolean>> cells;
+  private int x, y;
 
   public GameOfLife(int x, int y) {
     this.x = x;
     this.y = y;
+    this.cells = create_cells();
   }
 
   public int get_x() {
@@ -17,6 +21,62 @@ public class GameOfLife {
   }
 
   public boolean get_cell(int x, int y) {
-    return false;
+    return this.cells.get(x).get(y);
+  }
+
+  public void set_cell(int x, int y, boolean value) {
+    this.cells.get(x).set(y, value);
+  }
+
+  public void next_generation() {
+    ArrayList<ArrayList<Boolean>> new_cells = create_cells();
+
+    for (int ix = 0; ix < this.get_x(); ix++) {
+      for (int iy = 0; iy < this.get_y(); iy++) {
+        int neighbourhood = get_neighbourhood(ix, iy);
+
+        if (get_cell(ix, iy)) {
+          if (neighbourhood == 3 || neighbourhood == 2) {
+            new_cells.get(ix).set(iy, true);
+          }
+        } else {
+          if (neighbourhood == 3) {
+            new_cells.get(ix).set(iy, true);
+          }
+        }
+      }
+    }
+
+    this.cells = new_cells;
+  }
+
+  private int get_neighbourhood(int x, int y) {
+    int neighbourhood = 0;
+
+    for (int ix = (Math.max(x - 1, 0)); ix <= Math.min(x + 1, this.get_x() - 1); ix++) {
+      for (int iy = (Math.max(y - 1, 0)); iy <= Math.min(y + 1, this.get_y() - 1); iy++) {
+
+        if (ix == x && iy == y) {
+          continue;
+        }
+
+        if (this.get_cell(ix, iy)) {
+          neighbourhood += 1;
+        }
+      }
+    }
+    return neighbourhood;
+  }
+
+  private ArrayList<ArrayList<Boolean>> create_cells() {
+    ArrayList<ArrayList<Boolean>> cells = new ArrayList<ArrayList<Boolean>>();
+    for (int i = 0; i < this.get_x(); i++) {
+      ArrayList<Boolean> row = new ArrayList<Boolean>();
+      for (int j = 0; j < this.get_y(); j++) {
+        row.add(false);
+      }
+      cells.add(row);
+    }
+    return cells;
   }
 }
--- game_of_life/java/src/test/java/katas/GameOfLifeTest.java
+++ game_of_life/java/src/test/java/katas/GameOfLifeTest.java
@@ -23,4 +23,48 @@ public class GameOfLifeTest extends TestCase {
     GameOfLife gol = new GameOfLife(1, 1);
     assertFalse(gol.get_cell(0, 0));
   }
+
+  public void testSettingCellValue() {
+    GameOfLife gol = new GameOfLife(1, 1);
+    assertFalse(gol.get_cell(0, 0));
+    gol.set_cell(0, 0, true);
+    assertTrue(gol.get_cell(0, 0));
+  }
+
+  public void testRuleOne() {
+    GameOfLife gol = new GameOfLife(2, 2);
+    gol.set_cell(0, 0, true);
+    gol.next_generation();
+    assertFalse(gol.get_cell(0, 0));
+  }
+
+  public void testRuleTwoMoreThan3Neighbours() {
+    GameOfLife gol = new GameOfLife(3, 3);
+    gol.set_cell(0, 0, true);
+    gol.set_cell(1, 0, true);
+    gol.set_cell(0, 1, true);
+    gol.set_cell(1, 1, true);
+    gol.set_cell(1, 2, true);
+    gol.next_generation();
+    assertFalse(gol.get_cell(1, 1));
+  }
+
+  public void testRuleThreeTwoNeighbours() {
+    GameOfLife gol = new GameOfLife(2, 2);
+    gol.set_cell(0, 0, true);
+    gol.set_cell(1, 0, true);
+    gol.set_cell(0, 1, true);
+    gol.next_generation();
+    assertTrue(gol.get_cell(0, 0));
+  }
+
+  public void RuleFourDeadWithThreeNeighbours() {
+    GameOfLife gol = new GameOfLife(2, 2);
+    gol.set_cell(0, 0, true);
+    gol.set_cell(1, 0, true);
+    gol.set_cell(0, 1, true);
+    gol.set_cell(1, 1, false);
+    gol.next_generation();
+    assertTrue(gol.get_cell(1, 1));
+  }
 }
